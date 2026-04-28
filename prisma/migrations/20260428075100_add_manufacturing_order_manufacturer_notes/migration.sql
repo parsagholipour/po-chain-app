712ac5c347f429f78daf10a67d40804a106c247d
Parsa Gholipour <12parsa@gmail.com>
2026-04-28 07:53:25 +0400

    fix database issue

---
+++ prisma/migrations/20260428075100_add_manufacturing_order_manufacturer_notes/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "ManufacturingOrderManufacturer"
+ADD COLUMN "depositNote" TEXT,
+ADD COLUMN "manufacturingNote" TEXT,
+ADD COLUMN "balanceNote" TEXT,
+ADD COLUMN "readyNote" TEXT,
+ADD COLUMN "pickedUpNote" TEXT;
